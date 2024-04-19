c7fbb820e5c04e2c2e852ff6083cce46004f533a
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-04-19 10:43:43 +0700

    feat: ids

---
+++ Backend/go.mod
@@ -0,0 +1,9 @@
+module tubes2
+
+go 1.21.4
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.21.0 // indirect
+)
---
+++ Backend/ids1.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "strings"
+
+    "github.com/PuerkitoBio/goquery"
+)
+
+// Function to scrape links from a Wikipedia article
+func getLinksFromWikipedia(url string) []string {
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+
+    links := []string{}
+    if resp.StatusCode != 200 {
+        log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+    }
+
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    doc.Find("a[href^='/wiki/']").Each(func(i int, s *goquery.Selection) {
+        link, _ := s.Attr("href")
+        if !strings.Contains(link, ":") { // Exclude links with colons (administrative links)
+            links = append(links, link)
+        }
+    })
+
+    return links
+}
+
+// Function to find the shortest path using Iterative Deepening Search (IDS)
+func iterativeDeepeningSearch(startURL, goalURL string, maxDepth int) []string {
+    visited := make(map[string]bool)
+    queue := make([][]string, 0)
+    queue = append(queue, []string{startURL})
+
+    for len(queue) > 0 {
+        path := queue[0]
+        queue = queue[1:]
+
+        currentURL := path[len(path)-1]
+        if currentURL == goalURL {
+            return path
+        }
+
+        if len(path) <= maxDepth {
+            links := getLinksFromWikipedia("https://en.wikipedia.org" + currentURL)
+            for _, link := range links {
+                if !visited[link] {
+                    newPath := append([]string(nil), path...)
+                    newPath = append(newPath, link)
+                    queue = append(queue, newPath)
+                    visited[link] = true
+                }
+            }
+        }
+    }
+
+    return nil
+}
+
+func main() {
+    var startURL, goalURL string
+    fmt.Print("Enter the start Wikipedia article URL: ")
+    fmt.Scanln(&startURL)
+    fmt.Print("Enter the target Wikipedia article URL: ")
+    fmt.Scanln(&goalURL)
+
+    maxDepth := 10 // You can adjust this based on your preference
+
+    path := iterativeDeepeningSearch(startURL, goalURL, maxDepth)
+
+    if path != nil {
+        fmt.Println("Shortest path found:")
+        for _, link := range path {
+            fmt.Println("https://en.wikipedia.org" + link)
+        }
+    } else {
+        fmt.Println("No path found within the specified depth limit.")
+    }
+}
