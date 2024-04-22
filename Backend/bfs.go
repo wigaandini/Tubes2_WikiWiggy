9c66f82ebd270143992f34a4b728ef6252a5fc50
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-04-22 15:27:27 +0700

    feat: revise bfs

--- Backend/bfs.go
+++ Backend/bfs.go
@@ -1,5 +1,4 @@
 package main
-
 import (
 	"container/list"
 	"fmt"
@@ -7,16 +6,12 @@ import (
 	"log"
 	"strings"
 	"net/url"
-
 	"github.com/PuerkitoBio/goquery"
 )
-
-
 // Link Scraper
 func convertToURL(title string) string {
 	return fmt.Sprintf("https://en.wikipedia.org/wiki/%s", strings.ReplaceAll(title, " ", "_"))
 }
-
 func isValidArticleLink(link string) bool {
 	prefixes := []string{
 		"/wiki/Special:",
@@ -36,15 +31,12 @@ func isValidArticleLink(link string) bool {
 	}
 	return strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":")
 }
-
 func linkScraper(url string, visited map[string]bool) []string {
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var uniqueLinks []string
-
 	doc.Find("body a").Each(func(index int, item *goquery.Selection) {
 		linkTag := item
 		link, _ := linkTag.Attr("href")
@@ -53,18 +45,14 @@ func linkScraper(url string, visited map[string]bool) []string {
 			uniqueLinks = append(uniqueLinks, "https://en.wikipedia.org"+link)
 		}
 	})
-
 	return uniqueLinks
 }
-
-
 // Graph
 type Graph struct {
 	nodes   []*Node
 	adjList map[string][]string
 	visitedCount int
 }
-
 func NewGraph() *Graph {
 	return &Graph{
 		nodes:        []*Node{},
@@ -72,36 +60,28 @@ func NewGraph() *Graph {
 		visitedCount: 0,
 	}
 }
-
 type Node struct {
 	val string
 }
-
 func (g *Graph) AddNode(value string) *Node {
 	node := &Node{val: value}
 	g.nodes = append(g.nodes, node)
 	return node
 }
-
 func (g *Graph) AddEdge(node1, node2 string) {
 	g.adjList[node1] = append(g.adjList[node1], node2)
 	g.adjList[node2] = append(g.adjList[node2], node1)
 }
-
-
 // BFS
 func (g *Graph) BFS(start, end string) []string {
 	visited := make(map[string]bool)
 	parent := make(map[string]string)
 	q := list.New()
-
 	visited[start] = true
 	q.PushBack(start)
-
 	for q.Len() != 0 {
 		currentNode := q.Front().Value.(string)
 		q.Remove(q.Front())
-
 		if currentNode == end {
 			path := []string{}
 			current := end
@@ -111,7 +91,6 @@ func (g *Graph) BFS(start, end string) []string {
 			}
 			return path
 		}
-
 		for _, neighbor := range g.adjList[currentNode] {
 			g.visitedCount++
 			if !visited[neighbor] {
@@ -121,12 +100,13 @@ func (g *Graph) BFS(start, end string) []string {
 			}
 		}
 	}
-
 	return nil
 }
 
 // Main
 func main() {
+    startTitle := "Taylor Swift"
+	endTitle := "Fruit of the Loom"
 	var startTitle, goalTitle string
 	fmt.Printf("Enter the start title: ")
 	fmt.Scanln(&startTitle)
@@ -134,20 +114,18 @@ func main() {
 	fmt.Scanln(&goalTitle)
 
 	startURL := convertToURL(startTitle)
+	goalURL := convertToURL(endTitle)
 	goalURL := convertToURL(goalTitle)
 
     start := time.Now()
     g := NewGraph()
     visited := make(map[string]bool)
-
     visited[startURL] = true
     q := list.New()
     q.PushBack(startURL)
-
     for q.Len() != 0 {
         currentURL := q.Front().Value.(string)
         q.Remove(q.Front())
-
         links := linkScraper(currentURL, visited)
         for _, link := range links {
             g.AddEdge(currentURL, link)
@@ -168,26 +146,21 @@ func main() {
             q.PushBack(link)
         }
     }
-
     fmt.Println("No path found.")
 	fmt.Println("Length of Path: 0")
 	fmt.Println("Number of Articles Visited:", g.visitedCount)
     fmt.Println("BFS Time:", time.Since(start))
 }
-
 func getTitle(urlString string) (string) {
 	parsedURL, err := url.Parse(urlString)
 	if err != nil {
 		return ""
 	}
-
 	pathParts := strings.Split(parsedURL.Path, "/")
 	lastPart := pathParts[len(pathParts)-1]
-
 	title, err := url.PathUnescape(lastPart)
 	if err != nil {
 		return ""
 	}
-
 	return title
 }
--- Backend/go.mod
+++ Backend/go.mod
@@ -6,5 +6,6 @@ require github.com/PuerkitoBio/goquery v1.9.1
 
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	golang.org/x/net v0.21.0 // indirect
 )
